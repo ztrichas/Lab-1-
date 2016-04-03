18425de7e93219a19fadcf6f37dfa2440b4a2e01
Zach <ztrichas2019@pingry.org>
2016-04-03 11:16:03 -0400

    Initial Commit, putting input and outputs

---
+++ Lab 1/taxCalculator.py
@@ -0,0 +1,8 @@
+income = raw_input("Welcome to the tax calculator. \nHow much gross income did you earn last year?")
+
+print("Results for the 2000 plan. \nTaxes owed: ${} \nPercent of gross:{}% \nNet Income:${}"#.whatever)
+
+print("Results for the 2008 plan. \nTaxes owed: ${} \nPercent of gross:{}% \nNet Income:${}"#.whatever)
+
+print("Results for the 2014 plan. \nTaxes owed: ${} \nPercent of gross:{}% \nNet Income:${}"#.whatever)
+
